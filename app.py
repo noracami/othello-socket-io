2a4e0920e9ec95696635374093c362616cc86aba
noracami <kerke.2011@gmail.com>
2024-04-15 03:08:06 +0800

    ✨ add socket.io

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
+import os
+import socketio
 from flask import Flask
 
 app = Flask(__name__)
 
+REDIS_URI = os.environ.get("REDIS_URI", "redis://localhost:6379/0")
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+redis_manager = socketio.RedisManager(REDIS_URI)
+sio = socketio.Server(client_manager=redis_manager, cors_allowed_origins="*")
+app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app)
+
+
+@sio.event
+def connect(sid, environ):
+    print(f"connect {sid}")
+    sio.emit("message", "Hello, World!", room=sid)
+
+
+@sio.event
+def disconnect(sid):
+    print(f"disconnect {sid}")
+
+
+@app.route("/health")
+def health():
+    return "OK"
 
 
 if __name__ == "__main__":
-    app.run()
+    socketio.run(app, debug=True)
